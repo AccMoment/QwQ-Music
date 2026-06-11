ef738724eb3076b353634e3776c0edd646121449
metaone01 <metaone01@outlook.com>
2026-06-11 16:56:49 +0800

    fix: 修复了背景效果被裁剪的问题

--- QwQ Music/Program.cs
+++ QwQ Music/Program.cs
@@ -12,7 +12,7 @@ using ThreadState = System.Diagnostics.ThreadState;
 namespace QwQ_Music;
 
 public static class Program {
-    public static string VersionText => "2.1.2";
+    public static string VersionText => "2.1.3";
 
     [STAThread]
     public static async Task Main(string[] args) {
